30866c9de1efa6e1067bf3e242870bdd9347f39c
Saleh Albatati <0xack13@gmail.com>
2014-07-29 17:10:26 +0300

    Add youtube streamer route

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var exec, request, spawn, _ref;
 
 var app = express();
+request = require('request');
+_ref = require('child_process'), spawn = _ref.spawn, exec = _ref.exec;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +25,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.get('/:id', function(req, res) {
+    var entry = req.params.id;
+    return youtube_dl_url_child = exec("./youtube-dl --simulate --get-url http://www.youtube.com/watch?v=" + entry, function(err, stdout, stderr) {
+          var ffmpeg_child, youtube_dl_url;
+
+          youtube_dl_url = stdout.toString();
+
+          if(youtube_dl_url !== ""){
+              console.log ("url: " + youtube_dl_url);
+              youtube_dl_url = youtube_dl_url.substring(0, youtube_dl_url.length - 1);
+              res.contentType = 'audio/mpeg3';
+              ffmpeg_child = spawn("ffmpeg", ['-i', 'pipe:0', '-acodec', 'libmp3lame', '-f', 'mp3', '-']);
+              ffmpeg_child.stdout.pipe(res);
+
+              return request({
+                url: youtube_dl_url,
+                headers: {
+                  'Youtubedl-no-compression': 'True'
+                }
+              }).pipe(ffmpeg_child.stdin);
+          }else{
+            console.log ("error: " + stderr);
+            return false;
+          }
+        });
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -60,4 +89,4 @@ module.exports = app;
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
-});
+}); 
